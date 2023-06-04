79621b6ee4722210c3677a525e8bf67701ec6b71
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2023-06-03 20:38:43 -0400

    all posts

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,9 +1,33 @@
 import Layout from "@/components/Layout";
+import ListPosts from "@/components/ListPosts";
+import Post from "@/interface/Post";
+import { SectionPosts } from "@/styles/pages/posts";
+import { GetServerSideProps } from "next";
 
-export default function Posts() {
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <Layout>
-      <h1>Posts</h1>
+      <SectionPosts>
+        <h1>Todos os posts</h1>
+        <ListPosts posts={posts} />
+      </SectionPosts>
     </Layout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  posts: Post[];
+}> = async () => {
+  const res = await fetch("http://localhost:3000/api/posts/");
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ src/styles/pages/posts.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SectionPosts = styled.section`
+  width: 100%;
+  padding: 40px 80px;
+`;
