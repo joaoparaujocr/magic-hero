7fc33207467a6c2151c0e38559bef2059f31ad28
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2023-06-03 20:53:09 -0400

    apdate database

--- database/index.ts
+++ database/index.ts
@@ -4,48 +4,64 @@ const database = [
     title: "Capítulo 1: O Chamado Relâmpago",
     description:
       "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+    content:
+      "No reino mágico de Eldoria, onde seres místicos e criaturas encantadas vivem em harmonia, o poderoso Thundar, o Protetor do Trovão, é conhecido como o guardião da paz e da justiça. Ele é um guerreiro corajoso, cujo coração está repleto de bondade e determinação.\nEm uma noite tempestuosa, quando raios e trovões dançavam no céu, uma profecia antiga é revelada aos sábios da Cidade dos Sábios. A profecia falava sobre um grande mal que ameaçaria Eldoria, e somente Thundar, o escolhido, poderia detê-lo. Thundar sente o chamado em seu coração e parte em busca de respostas.",
   },
   {
     id: 2,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 2: A Jornada",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "Thundar embarca em uma jornada perigosa e emocionante por terras desconhecidas",
+    content:
+      "Thundar embarca em uma jornada perigosa e emocionante por terras desconhecidas. Ele encontra desafios pelo caminho, como criaturas sombrias e terras traiçoeiras. No entanto, sua coragem e habilidade com a espada o ajudam a superar essas adversidades.\nDurante sua jornada, Thundar conhece aliados leais, como a sábia feiticeira Luna, que possui o poder de controlar a luz e a escuridão. Juntos, eles se tornam uma equipe formidável, determinada a proteger Eldoria e derrotar o mal que se aproxima.",
   },
   {
     id: 3,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 3: O Despertar do Mal",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "Thundar e Luna descobrem que um antigo feiticeiro malévolo, conhecido como Drakonar, está por trás da ameaça a Eldoria.",
+    content:
+      "Thundar e Luna descobrem que um antigo feiticeiro malévolo, conhecido como Drakonar, está por trás da ameaça a Eldoria. Drakonar busca uma antiga relíquia que pode conceder a ele poderes inimagináveis e mergulhar o reino na escuridão eterna.\nThundar e Luna correm contra o tempo para encontrar a relíquia antes que caia nas mãos erradas. Eles enfrentam criaturas das trevas e desvendam segredos ancestrais para desvendar a localização da relíquia.",
   },
   {
     id: 4,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 4: A Batalha nas Profundezas",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "Thundar e Luna adentram as profundezas de uma montanha proibida, onde a relíquia está escondida.",
+    content:
+      "Thundar e Luna adentram as profundezas de uma montanha proibida, onde a relíquia está escondida. Lá, eles enfrentam uma série de desafios perigosos, armadilhas mortais e guardiões poderosos que protegem o artefato.\nCom sua inteligência e habilidades mágicas combinadas, Thundar e Luna superam os obstáculos e finalmente alcançam a relíquia. No entanto, sua vitória é breve, pois Drakonar aparece, pronto para lutar.",
   },
   {
     id: 5,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 5: O Confronto Final",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "A batalha épica entre Thundar, Luna e Drakonar irrompe na montanha proibida. Raios e trovões rugem pelo céu enquanto os dois lados lutam com todas as suas forças.",
+    content:
+      "A batalha épica entre Thundar, Luna e Drakonar irrompe na montanha proibida. Raios e trovões rugem pelo céu enquanto os dois lados lutam com todas as suas forças. Thundar desencadeia seu poder do trovão, canalizando sua energia para lançar ataques poderosos.\nLuna usa sua magia para criar escudos protetores e lançar feitiços ofensivos contra Drakonar. A batalha é feroz e intensa, testando a coragem e a determinação dos heróis.",
   },
   {
     id: 6,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 6: O Poder da Amizade",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "Apesar de seus melhores esforços, Thundar e Luna são superados pela magia negra de Drakonar.",
+    content:
+      "Apesar de seus melhores esforços, Thundar e Luna são superados pela magia negra de Drakonar. Parece que tudo está perdido, mas então, eles são lembrados do verdadeiro poder que possuem: a amizade e a crença na bondade.\nUnindo suas forças, Thundar e Luna criam um escudo de luz que repele os ataques sombrios de Drakonar. Eles se erguem novamente, mais fortes do que nunca, determinados a proteger Eldoria e seus habitantes.",
   },
   {
     id: 7,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 7: A Vitória",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "Com suas habilidades e trabalho em equipe aprimorados, Thundar e Luna finalmente conseguem derrotar Drakonar.",
+    content:
+      "Com suas habilidades e trabalho em equipe aprimorados, Thundar e Luna finalmente conseguem derrotar Drakonar. O malévolo feiticeiro é selado em uma dimensão escura, incapaz de causar mais danos ao reino mágico.\nA notícia de sua vitória se espalha rapidamente por Eldoria, e as criaturas encantadas celebram o retorno da paz. Thundar e Luna são aclamados como heróis e recebem o respeito e a gratidão de todos.",
   },
   {
     id: 8,
-    title: "Capítulo 1: O Chamado Relâmpago",
+    title: "Capítulo 8: O Legado",
     description:
-      "Thundar, um jovem aprendiz de mago, recebe um chamado dos céus para se tornar o Protetor do Trovão.",
+      "Thundar e Luna se tornam guardiões de Eldoria, trabalhando juntos para manter o reino seguro.",
+    content:
+      "Thundar e Luna se tornam guardiões de Eldoria, trabalhando juntos para manter o reino seguro. Eles estabelecem uma academia de treinamento, onde novos aprendizes são instruídos nas artes da magia e da guerra, preparando-se para proteger o mundo mágico no futuro.\nA história de Thundar, o Protetor do Trovão, e sua jornada épica é contada de geração em geração, inspirando aqueles que a ouvem a acreditar na força do bem e a lutar por justiça e harmonia. Eldoria prospera, graças ao coração nobre e à bravura de Thundar.",
   },
 ];
 
