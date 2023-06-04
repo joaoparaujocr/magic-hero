414e50ec89fa0c45f7b4515e78c81d52bb730568
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2023-06-03 21:13:10 -0400

    page about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,31 @@
 import Layout from "@/components/Layout";
+import { AboutContainer } from "@/styles/pages/about";
 
 export default function About() {
   return (
     <Layout>
-      <h1>Sobre</h1>
+      <AboutContainer>
+        <h1>Sobre</h1>
+        <p>
+          Bem-vindo(a) à página sobre a incrível história de Thundar, o Protetor
+          do Trovão!
+          <br />
+          <br />
+          Em um mundo mágico chamado Eldoria, onde criaturas encantadas e seres
+          místicos vivem em harmonia, Thundar emergiu como um lendário herói.
+          <br />
+          <br />
+          Conhecido como o guardião da paz e da justiça, Thundar é um guerreiro
+          corajoso, cujo coração está repleto de bondade e determinação.
+          <br />
+          <br />
+          Em uma noite tempestuosa, quando raios e trovões dançavam no céu,
+          Thundar ouviu o chamado de uma antiga profecia. Ele foi escolhido para
+          enfrentar um grande mal que ameaçava Eldoria. Assim, ele partiu em uma
+          jornada perigosa e emocionante, encontrando aliados leais, como a
+          sábia feiticeira Luna, e enfrentando inúmeros desafios pelo caminho.
+        </p>
+      </AboutContainer>
     </Layout>
   );
 }
---
+++ src/styles/pages/about.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.section`
+  max-width: 900px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding: 40px 80px;
+
+  p {
+    white-space: pre-wrap;
+  }
+`;
